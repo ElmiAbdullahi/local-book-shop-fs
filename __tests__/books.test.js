b7a9025f479faeb642d32ebd8678a0e0f4a73d92
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-11 14:58:26 -0800

    test built for books list

---
+++ __tests__/books.test.js
@@ -0,0 +1,19 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should return list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(7);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
