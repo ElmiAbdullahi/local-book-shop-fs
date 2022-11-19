0cc8996fa6732ee7af98cdf2c92f65ce72790834
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-18 16:00:06 -0800

    fixed test for get/books route

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -11,7 +11,22 @@ describe('books routes', () => {
   it('/books should return list of books', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(7);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+    });
   });
+
+  // it('/books/1 should return a book detail with authors', async () => {
+  //   const res = await request(app).get('/books/1');
+  //   console.log(res.body);
+  //   expect(res.body).toEqual({
+  //     title: expect.any(String),
+  //     released: expect.any(Number),
+  //     authors: expect.any(String),
+  //   });
+  // });
 
   afterAll(() => {
     pool.end();
