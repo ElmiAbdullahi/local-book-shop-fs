45de96512936cf4fceee78f631da23a58fbac854
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-18 19:04:27 -0800

    author test passing

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -1,21 +1,20 @@
-const pool = require('../lib/utils/pool.js');
-const setup = require('../data/setup.js');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('authors route', () => {
+describe('authors and books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-});
 
-it('/authors should return list of book', async () => {
-  const res = await request(app).get('/authors');
-  expect(res.body.length).toEqual(5);
-  expect(res.body[0]).toEqual({
-    id: expect.any(String),
-    name: expect.any(String),
-    dob: expect.any(Number),
-    pob: expect.any(String),
+  it('/authors should return a list of authors', async () => {
+    const resp = await request(app).get('/authors');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(5);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
   });
 });
